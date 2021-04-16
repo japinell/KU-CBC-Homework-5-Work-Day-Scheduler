01e512a9f036414befb00919e172ea2d4fa3ad0d
JAPinell <japinell@yahoo.com>
2021-04-16 18:07:18 -0500

    Added p-element to display success/error messages, removed margins/paddings, and improved responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Timeblock elements
 const containerDiv = $(".container");
-const TIME_BLOCKS_START = 9;
+const TIME_BLOCKS_START = 15;
 const TIME_BLOCKS_NUMBER = 9;
 
 // Objects
@@ -20,6 +20,34 @@ var currentTimeText = Number(today.format("hA"));
 //
 $("#currentDay").text(today.format("dddd, MMM Do"));
 
+// Display success status message
+function ShowSuccess(message) {
+  //
+  var pEl = $("p.description");
+  pEl.addClass("text-success").text(message);
+  //
+  setTimeout(function () {
+    //
+    pEl.text("");
+    //
+  }, 1000);
+  //
+}
+
+// Display error status message
+function ShowError(message) {
+  //
+  var pEl = $("p.description");
+  pEl.addClass("text-danger").text(message);
+  //
+  setTimeout(function () {
+    //
+    pEl.text("");
+    //
+  }, 1000);
+  //
+}
+
 // Load schedule from localStorage
 function LoadScheduleFromLocalStorage() {
   //
@@ -28,7 +56,9 @@ function LoadScheduleFromLocalStorage() {
   );
   //
   if (savedWorkDaySchedule != null) {
+    //
     workDaySchedule = savedWorkDaySchedule;
+    //
   }
   //
 }
@@ -66,6 +96,8 @@ function SaveScheduleToLocalStorage() {
   //
   localStorage.setItem("WorkDaySchedule", JSON.stringify(workDaySchedule));
   //
+  ShowSuccess("Schedule successfully saved to local storage...");
+  //
 }
 
 // Check if the item is already in the workDaySchedule array
@@ -91,7 +123,7 @@ function IsTimeBlockBooked(newTime) {
 // Style time blocks with past, present, and future classes
 function StyleTimeBlocks() {
   //
-  var timeBlockId, timeBlockVal, textAreaEl;
+  var timeBlockId, timeBlockVal, textAreaEl, saveBtn;
   //
   // Select the timeblock container and children with class = "time-block"
   //
@@ -102,12 +134,14 @@ function StyleTimeBlocks() {
       timeBlockId = $(this).attr("time-block-id");
       timeBlockVal = $(this).attr("time-block-value");
       textAreaEl = $(this).children(".form-floatting").children(".textarea");
+      saveBtn = $(this).children(".row").children(".btn");
       //
       //if (timeBlockVal < currentTime || timeBlockVal == 12) {
       if (timeBlockVal < currentTime) {
         //
         textAreaEl.addClass("past");
         textAreaEl.attr("disabled", true);
+        saveBtn.attr("disabled", true);
         //
       } else if (timeBlockVal == currentTime) {
         //
@@ -164,7 +198,13 @@ function RenderTimeBlocks() {
   //
   var time = today.hour(TIME_BLOCKS_START);
   var timeVal, timeText;
-  var rowEl, colEl, textAreaEl, saveBtnEl, iEl;
+  var rowEl, colEl, textAreaEl, saveBtnEl, iEl, pEl;
+
+  // Message element
+  pEl = $("<p>");
+  pEl.addClass("h6 text-center text-muted font-italic description");
+  pEl.text("Hello");
+  pEl.appendTo(containerDiv);
 
   // containerDiv.addClass("container-sm");
 
@@ -178,18 +218,18 @@ function RenderTimeBlocks() {
     // rowEl.attr("id", timeText);
     rowEl.attr("time-block-id", timeText);
     rowEl.attr("time-block-value", timeVal);
-    rowEl.addClass("row time-block flex-row");
+    rowEl.addClass("row time-block");
 
     // Columns
     // Hour
     colEl = $("<div>");
-    colEl.addClass("col-xl-1 col-sm-2 col-3 text-right row hour");
+    colEl.addClass("col-1 mx-0 px-0 text-center row hour");
     colEl.text(timeText);
     colEl.appendTo(rowEl);
 
     // Text area
     colEl = $("<div>");
-    colEl.addClass("col-xl-10 col-sm-8 col-6 form-floatting row px-0");
+    colEl.addClass("col-10 mx-0 px-0 form-floatting row");
 
     textAreaEl = $("<textarea>");
     textAreaEl.addClass("form-control text-dark textarea");
@@ -200,7 +240,7 @@ function RenderTimeBlocks() {
 
     // Save button
     colEl = $("<div>");
-    colEl.addClass("col-xl-1 col-sm-2 col-3 text-left align-self-center row");
+    colEl.addClass("col-1 mx-0 px-0 text-left align-self-center row");
 
     saveBtnEl = $("<button>");
     saveBtnEl.addClass("btn btn-lg btn-block saveBtn");
@@ -228,6 +268,7 @@ function InitializeSchedule() {
   RenderTimeBlocks();
   RenderBookedTimeBlocks();
   StyleTimeBlocks();
+  ShowSuccess("Application successfully initialized...");
   //
 }
 
